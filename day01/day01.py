bd13d295fc6f99781d0b306c8a3298efaa9e09bf
Stanislav Rusak <stanislav.rusak@gmail.com>
2022-12-03 09:25:37 +0100

    fix folder structure

--- day01/day01.py
+++ day01/day01.py
@@ -1,38 +1,17 @@
-SHAPES = {'X': 1, 'Y': 2, 'Z':3}
-OUTCOMES = {
-                "AX": 3, "AY": 6, "AZ": 0,
-                "BX": 0, "BY": 3, "BZ": 6,
-                "CX": 6, "CY": 0, "CZ": 3,
-           }
+with open("input.txt", "r") as f:
+    inp = f.read().strip()
+    elves = inp.split("\n\n")
 
-MAP = {
-        "AX": "AZ", "AY": "AX", "AZ": "AY",
-        "BX": "BX", "BY": "BY", "BZ": "BZ",
-        "CX": "CY", "CY": "CZ", "CZ": "CX",
-        }
+def get_calories(elf):
+    return sum([int(num) for num in elf.split('\n')])
 
-def main():
-
-    with open("input.txt", "r") as f:
-        strategy = [item[0] + item[2] for item in f.readlines()]
-
-    # part 1: strategy -> outcomes
-    score1 = get_score(strategy)
-
-    # part 2 strategy -> part 1 strategy -> outcomes
-    strategy2 = map(lambda x: MAP[x], strategy)
-    score2 = get_score(strategy2)
-
-    print(f"Part 1: {score1}")
-    print(f"Part 2: {score2}")
-
-def get_score(strategy):
+calories_list = []
+for elf in elves:
     
-    score = 0
-    for item in strategy:
-        score += OUTCOMES[item] + SHAPES[item[1]]
-    
-    return score
+    calories =  get_calories(elf)
+    calories_list.append(calories)
 
-if __name__ == "__main__":
-    main()
+calories_list.sort()
+
+print(f"Top elf has {calories_list[-1]} calories.")
+print(f"Top 3 elves have {sum(calories_list[-3:])} calories.")
