02da64146765652351ee53628072895bf71575fa
Stanislav Rusak <stanislav.rusak@gmail.com>
2022-12-04 06:39:56 +0100

    dried up

--- day04/day04.py
+++ day04/day04.py
@@ -10,32 +10,23 @@ def main():
         elf1, elf2 = line.split(',')
         elf1, elf2 = elf1.split('-'), elf2.split('-')     
 
-        if contained(elf1, elf2):
+        if check(elf1, elf2, "contain"):
             coutained_count += 1
 
-        if overlap(elf1, elf2):
+        if check(elf1, elf2, "overlap"):
             overlap_count += 1
 
     print(f"Part 1: {coutained_count}")
     print(f"Part 2: {overlap_count}")
 
+def check(elf1, elf2, condition):
 
-def contained(elf1, elf2):
+    index = {"contain": 1, "overlap": 0}[condition]
 
-    if int(elf1[0]) >= int(elf2[0]) and int(elf1[1]) <= int(elf2[1]):
+    if int(elf1[0]) >= int(elf2[0]) and int(elf1[index]) <= int(elf2[1]):
         return True
          
-    elif int(elf2[0]) >= int(elf1[0]) and int(elf2[1]) <= int(elf1[1]):
-        return True
-    
-    return False
-
-def overlap(elf1, elf2):
-
-    if int(elf1[0]) >= int(elf2[0]) and int(elf1[0]) <= int(elf2[1]):
-        return True
-         
-    elif int(elf2[0]) >= int(elf1[0]) and int(elf2[0]) <= int(elf1[1]):
+    elif int(elf2[0]) >= int(elf1[0]) and int(elf2[index]) <= int(elf1[1]):
         return True
     
     return False
