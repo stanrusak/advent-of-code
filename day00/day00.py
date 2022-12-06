859f39733be75795f6b6f516d1e2048d47cafcd7
Stanislav Rusak <stanislav.rusak@gmail.com>
2022-12-06 15:36:23 +0100

    added testing template

--- day00/day00.py
+++ day00/day00.py
@@ -1,8 +1,29 @@
+import pytest
+
+TEST_DATA = """\
+
+"""
+EXPECTED = 0, None
+
 with open("input.txt", "r") as f:
     data = f.read().strip()
 
-def main():
-    pass
+def main(data):
+    
+    part1 = 0
+    part2 = None
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+
+@pytest.mark.parametrize(
+    ('input_data','output'),
+    (
+        (TEST_DATA, EXPECTED),
+    )
+)
+def test_main(input_data, output):
+    assert main(input_data) == output
 
 if __name__ == "__main__":
-    main()
+    main(data)
